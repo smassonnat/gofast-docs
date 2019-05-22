4953da2b2a1b3138d9d6be8259f1b7d96c1c68f8
Sandra <sandra.massonnat@ceo-vision.com>
2019-05-22 13:21:03 +0200

    Remove build/html from git

--- conf.py
+++ conf.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import os
-
 from __future__ import division, print_function, unicode_literals
 
 from datetime import datetime
 
 from recommonmark.parser import CommonMarkParser
+
+import os
 
 extensions = []
 templates_path = ['/home/docs/checkouts/readthedocs.org/readthedocs/templates/sphinx', 'templates', '_templates', '.templates']
@@ -35,14 +35,19 @@ locale_dirs = [
     'locale/',
 ]
 gettext_compact = False
+gettext_uuid = True
+
+
+#os.system('sphinx-build -b html -D language=en . _build/html/en')
 
 #BASEDIR = os.path.dirname(os.path.abspath(__file__))
 
-def source_read_handler(app, docname, source):
-#    os.system('sphinx-build -b html -D language=en . 'os.path.join(BASEDIR,_build/html/en')
+#def build_finished(app, exception):
+#    os.system('sphinx-build -b html -D language=en . _build/html/en')
+#    print('do something here...')
 
-def setup(app):
-    app.connect('env-before-read-docs', source_read_handler)
+#def setup(app):
+ #   app.connect('build-finished', build_finished)
 	
 #make gettext
 #sphinx-intl update -p locale en
